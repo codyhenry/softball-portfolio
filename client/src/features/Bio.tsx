1ba5d3b33b8c88c5dc668d61f1b9e882e319dec4
codyhenry <codyhenry527@gmail.com>
2023-12-27 12:49:30 -0500

    removed schools array from bio

--- client/src/features/Bio.tsx
+++ client/src/features/Bio.tsx
@@ -1,7 +1,3 @@
-const schools = [
-  { team: "St. Thomas Aquinas Raiders", position: "SS", number: 10 },
-  { team: "Coral Springs Tigers", position: "P", number: 10 },
-];
 export function Bio() {
   return <h1>Christina D'Agostino</h1>;
 }
