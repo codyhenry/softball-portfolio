1dda6f5356440ce2de6f86a7924be68905f1f250
codyhenry <codyhenry527@gmail.com>
2023-12-27 12:44:24 -0500

    add schools for testing

--- client/src/features/Bio.tsx
+++ client/src/features/Bio.tsx
@@ -1,3 +1,7 @@
+const schools = [
+  { team: "St. Thomas Aquinas Raiders", position: "SS", number: 10 },
+  { team: "Coral Springs Tigers", position: "P", number: 10 },
+];
 export function Bio() {
   return <h1>Christina D'Agostino</h1>;
 }
