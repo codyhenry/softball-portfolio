aded31c7d81899405434e84bc15392caa97536ba
codyhenry <codyhenry527@gmail.com>
2023-12-30 13:01:39 -0500

    init stats component

---
+++ client/test/Stats.test.tsx
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom/vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+import { Stats } from "../src/features/Stats";
