9b4b239257b7524a7655dcf9cb73d257e8c165a1
codyhenry <codyhenry527@gmail.com>
2023-12-31 14:11:46 -0500

    stats renders tables

--- client/src/features/Stats.tsx
+++ client/src/features/Stats.tsx
@@ -1,1 +1,49 @@
-export function Stats() {}
+import { useState, useEffect } from "react";
+
+function DesktopStats() {
+  return (
+    <>
+      <h2>Batting</h2>
+      <table />
+      <h2>Baserunning</h2>
+      <table />
+      <h2>Fielding</h2>
+      <table />
+    </>
+  );
+}
+
+export function Stats() {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 990);
+
+  useEffect(() => {
+    window.addEventListener("resize", () =>
+      setIsDesktop(window.innerWidth > 990)
+    );
+    return () =>
+      window.removeEventListener("resize", () =>
+        setIsDesktop(window.innerWidth > 990)
+      );
+  });
+  return <h1>Player Statistics</h1>;
+  // {isDesktop &&}
+}
+
+<table>
+  <thead>
+    <tr>
+      <th>Team</th>
+      <th>Position</th>
+      <th>Number</th>
+    </tr>
+  </thead>
+  <tbody>
+    {schools.map((school) => (
+      <tr key={school.team}>
+        <td>{school.team}</td>
+        <td>{school.position}</td>
+        <td>{school.number}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>;
--- client/test/Stats.test.tsx
+++ client/test/Stats.test.tsx
@@ -3,3 +3,17 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import { Stats } from "../src/features/Stats";
+
+describe("Stats", () => {
+  it("renders 'Player Statistics' as a header", () => {});
+  it("renders a table for batting", () => {});
+  it("renders a table for fielding", () => {});
+  it("renders a table for base running", () => {});
+  it("indicates team leader in a particular stat");
+  it("renders as a slide for mobile devices");
+  it("shows one stat table at a time for mobile devices");
+  it("transitions between tables with a swipe on mobile devices");
+  it("transitions between tables with a button on mobile devices");
+  it("shows all three stat tables for desktop devices");
+  it("displays full stat name on hover");
+});
