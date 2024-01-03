71d317e55ece6ef16589e5907e2c55931ec0fc1e
codyhenry <codyhenry527@gmail.com>
2024-01-03 18:35:48 -0500

    stats renders component based on screen size

--- client/src/features/Stats.tsx
+++ client/src/features/Stats.tsx
@@ -1,21 +1,94 @@
 import { useState, useEffect } from "react";
+import { getTables } from "../test/jsonParser";
 
-function DesktopStats() {
+// overflowX - scroll (desktop)
+// overflowY - scroll (mobile)
+
+// TODO make a interface file to import from
+
+interface Table {
+  category: string;
+  stats: { [key: string]: { full: string; value: string; best: boolean } };
+}
+
+function StatsTable({
+  stats,
+  isDesktop,
+}: {
+  stats: { [key: string]: { full: string; value: string; best: boolean } };
+  isDesktop: boolean;
+}) {
   return (
-    <>
-      <h2>Batting</h2>
-      <table />
-      <h2>Baserunning</h2>
-      <table />
-      <h2>Fielding</h2>
-      <table />
-    </>
+    <div>
+      {isDesktop ? (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(stats).map((stat) => (
+                // on hover change to full
+                <th key={stat}>{stat}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {Object.keys(stats).map((stat) => (
+                <td key={stat}>{stats[stat].value}</td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <table>
+          <tbody>
+            {Object.keys(stats).map((stat) => (
+              <tr key={stat}>
+                {/* on hover change to full */}
+                <th>{stat}</th>
+                <td>{stats[stat].value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
+}
+
+// make swipeable
+function Carousel({ tables }: { tables: Array<Table> }) {
+  const [tableIndex, setTableIndex] = useState(0);
+  const handleNext = () => setTableIndex((tableIndex + 1) % tables.length);
+  // there are 3 tables
+  const handlePrev = () =>
+    setTableIndex((tables.length + tableIndex - 1) % tables.length);
+
+  return (
+    <div>
+      <button type="button" onClick={handlePrev}>
+        Prev
+      </button>
+      <h2>{tables[tableIndex].category}</h2>
+      <StatsTable stats={tables[tableIndex].stats} isDesktop={false} />
+      <button type="button" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+}
+
+function DesktopStats({ tables }: { tables: Array<Table> }) {
+  return tables.map((table) => (
+    <div key={table.category} data-testid="desktop-component">
+      <h2>{table.category}</h2>
+      <StatsTable stats={table.stats} isDesktop />
+    </div>
+  ));
 }
 
 export function Stats() {
   const [isDesktop, setIsDesktop] = useState(window.innerWidth > 990);
-
+  const tables = getTables();
   useEffect(() => {
     window.addEventListener("resize", () =>
       setIsDesktop(window.innerWidth > 990)
@@ -25,25 +98,14 @@ export function Stats() {
         setIsDesktop(window.innerWidth > 990)
       );
   });
-  return <h1>Player Statistics</h1>;
-  // {isDesktop &&}
+  return (
+    <>
+      <h1>Player Statistics</h1>
+      {isDesktop ? (
+        <DesktopStats tables={tables} />
+      ) : (
+        <Carousel tables={tables} />
+      )}
+    </>
+  );
 }
-
-<table>
-  <thead>
-    <tr>
-      <th>Team</th>
-      <th>Position</th>
-      <th>Number</th>
-    </tr>
-  </thead>
-  <tbody>
-    {schools.map((school) => (
-      <tr key={school.team}>
-        <td>{school.team}</td>
-        <td>{school.position}</td>
-        <td>{school.number}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>;
--- client/test/Stats.test.tsx
+++ client/test/Stats.test.tsx
@@ -1,19 +1,39 @@
 import "@testing-library/jest-dom/vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import { Stats } from "../src/features/Stats";
 
 describe("Stats", () => {
-  it("renders 'Player Statistics' as a header", () => {});
-  it("renders a table for batting", () => {});
-  it("renders a table for fielding", () => {});
-  it("renders a table for base running", () => {});
-  it("indicates team leader in a particular stat");
-  it("renders as a slide for mobile devices");
-  it("shows one stat table at a time for mobile devices");
+  it("renders 'Player Statistics' as a header", () => {
+    render(<Stats />);
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      "Player Statistics"
+    );
+  });
+  it("renders all 3 tables at a time for screens larger than 990px", () => {
+    render(<Stats />);
+    window.innerWidth = 1000;
+    fireEvent(window, new Event("resize"));
+    expect(screen.getAllByRole("table")).toHaveLength(3);
+  });
+  it("renders a header for each table", () => {
+    render(<Stats />);
+    const categories = screen.getAllByRole("heading", { level: 2 });
+    expect(categories.map((category) => category.textContent)).toEqual([
+      "Batting",
+      "Baserunning",
+      "Fielding",
+    ]);
+  });
+  it("shows one stat table at a time for screens smaller than 990px", () => {
+    render(<Stats />);
+    window.innerWidth = 700;
+    fireEvent(window, new Event("resize"));
+    expect(screen.getAllByRole("table")).toHaveLength(1);
+  });
   it("transitions between tables with a swipe on mobile devices");
   it("transitions between tables with a button on mobile devices");
-  it("shows all three stat tables for desktop devices");
+  it("indicates team leader in a particular stat");
   it("displays full stat name on hover");
 });
