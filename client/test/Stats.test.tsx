de5b36f2c8bd3422c14e89a2ac2092ff60738710
codyhenry <codyhenry527@gmail.com>
2024-01-06 13:17:44 -0500

    dry'd screen size change

--- client/test/Stats.test.tsx
+++ client/test/Stats.test.tsx
@@ -5,6 +5,10 @@ import { describe, expect, it } from "vitest";
 import { Stats } from "../src/features/Stats";
 
 describe("Stats", () => {
+  const initMobile = () => {
+    window.innerWidth = 700;
+    fireEvent(window, new Event("resize"));
+  };
   it("renders 'Player Statistics' as a header", () => {
     render(<Stats />);
     expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
@@ -28,12 +32,20 @@ describe("Stats", () => {
   });
   it("shows one stat table at a time for screens smaller than 990px", () => {
     render(<Stats />);
-    window.innerWidth = 700;
-    fireEvent(window, new Event("resize"));
+    initMobile();
     expect(screen.getAllByRole("table")).toHaveLength(1);
   });
+  it("transitions between tables with a button on mobile devices", () => {
+    render(<Stats />);
+    initMobile();
+    const nextButton = screen.getByRole("button", { name: "Next" });
+    fireEvent.click(nextButton);
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Baserunning"
+    );
+  });
+
   it("transitions between tables with a swipe on mobile devices");
-  it("transitions between tables with a button on mobile devices");
   it("indicates team leader in a particular stat");
-  it("displays full stat name on hover");
+  it("displays full stat name tooltip on hover");
 });
