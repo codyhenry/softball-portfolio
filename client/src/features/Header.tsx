f35ec7e7662e94517386b07e55d81a483233e913
codyhenry <codyhenry527@gmail.com>
2024-02-10 16:38:53 -0500

    init basic header

---
+++ client/src/features/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <div id="navbar">
+      <a href="#Bio">Bio</a>
+      <a href="#Highlights">Highlights</a>
+      <a href="#Stats">Stats</a>
+      <p>555-555-5555</p>
+    </div>
+  );
+}
